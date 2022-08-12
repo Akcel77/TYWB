83597b7c46d422673ad7c02e25d40e9839cb72cf
Akcel77 <Akeucel@gmail.com>
2022-08-12 17:44:22 +0200

    RideCrudController create

---
+++ src/Controller/Admin/RideCrudController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Ride;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class RideCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Ride::class;
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('title'),
+            SlugField::new('slug')->setTargetFieldName('title'),
+            DateTimeField::new('departureDate', 'Départ le'),
+            DateTimeField::new('arrivalDate', 'arrivée le'),
+            IntegerField::new('maxWeight', 'Poids max'),
+            ImageField::new('illustration')
+                ->setBasePath('uploads/')
+                ->setUploadDir('public/uploads/')
+                ->setUploadedFileNamePattern('[randomhash].[extension]')
+                ->setRequired(false),
+            TextField::new('subtitle'),
+            TextAreaField::new('description')->hideOnIndex(),
+            MoneyField::new('price')->setCurrency('EUR'),
+            AssociationField::new('category'),
+        ];
+    }
+
+}
