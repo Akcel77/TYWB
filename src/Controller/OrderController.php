a5797b94f6a2eb788e5d8fca0133b419de34abca
Akcel77 <Akeucel@gmail.com>
2022-08-18 16:22:19 +0200

    Work on summary order add()

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Classe\Cart;
 use App\Entity\Order;
+use App\Entity\OrderDetails;
 use App\Form\OrderType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -45,72 +46,59 @@ class OrderController extends AbstractController
         ]);
     }
 
-//    /**
-//     * @Route("/order/summary", name="order_recap")
-//     */
-//    public function add(Cart $cart, Request $request): Response
-//    {
-//        $form = $this->createForm(OrderType::class, null, [
-//            'user' => $this->getUser()
-//        ]);
-//
-//        $form->handleRequest($request);
-//
-//        //verifie si la commande est existante
-//        if ($form->isSubmitted() && $form->isValid()) {
-//            //initialisation de la date
-//            $date = new \DateTimeImmutable();
-//
-//
-//            $moto = $form->get('motos')->getData();
-//            $moto_content = $delivery->getFirstname().' '.$delivery->getLastname();
-//            $delivery_content .= '<br/>'.$delivery->getPhone();
-//
-//
-//            if ($delivery->getCompany()) {
-//                $delivery_content .= '<br/>'.$delivery->getCompany();
-//            }
-//            $delivery_content .= '<br/>'.$delivery->getAddress();
-//            $delivery_content .= '<br/>'.$delivery->getPostal().' '.$delivery->getCity();
-//            $delivery_content .= '<br/>'.$delivery->getCountry();
-//
-//            //enregistrer ma commande Order()
-//            $order = new Order();
-//            $reference = $date->format('dmY').'-'.uniqid();
+    /**
+     * @Route("/order/summary", name="order_recap")
+     */
+    public function add(Cart $cart, Request $request): Response
+    {
+        $form = $this->createForm(OrderType::class, null, [
+            'user' => $this->getUser()
+        ]);
+
+        $form->handleRequest($request);
+
+        //verifie si la commande est existante
+        if ($form->isSubmitted() && $form->isValid()) {
+            //initialisation de la date
+            $date = new \DateTimeImmutable();
+
+
+            $moto = $form->get('motos')->getData();
+            $moto_weight = $moto->getWeight();
+            $moto_content = $moto->getBrand().' '.$moto->getModel();
+            $moto_content .= '<br/>'.$moto->getMatriculation();
+            $moto_content .= '<br/>'. $moto->getWeight().' kg';
+
+            //enregistrer ma commande Order()
+            $order = new Order();
+            $reference = $date->format('dmY').'-'.uniqid();
 //            $order->setReference($reference);
-//            $order->setUser($this->getUser());
-//            $order->setCreatedAt($date);
-//            $order->setCarrierName($carriers->getName());
-//            $order->setCarrierPrice($carriers->getPrice());
-//            $order->setDelivery($delivery_content);
-//            $order->setState(0);
-//
-//            $this->entityManager->persist($order);
-//
-//            //enregistrer mes produits OrderDetails()
-//            foreach ($cart->getFull() as $product) {
-//                $orderDetails = new OrderDetails();
-//                $orderDetails->setMyOrder($order);
-//                $orderDetails->setProduct($product['product']->getName());
-//                $orderDetails->setQuantity($product['quantity']);
-//                $orderDetails->setPrice($product['product']->getPrice());
-//                $orderDetails->setTotal($product['product']->getPrice() * $product['quantity']);
-//                $this->entityManager->persist($orderDetails);
-//            }
-//
-//
-//
-//            $this->entityManager->flush();
-//
-//            return $this->render('order/add.html.twig', [
-//                'cart' => $cart->getFull(),
-//                'carrier' => $carriers,
-//                'delivery' => $delivery_content,
+            $order->setUser($this->getUser());
+            $order->setCreatedAt($date);
+            $order->setMoto($moto_content);
+            $order->setIsPaid(0);
+
+            $this->entityManager->persist($order);
+
+            //enregistrer mes produits OrderDetails()
+            foreach ($cart->getFull() as $ride) {
+                $orderDetails = new OrderDetails();
+                $orderDetails->setMyOrder($order);
+                $orderDetails->setRide($ride['ride']->getTitle());
+                $orderDetails->setPrice($ride['ride']->getPrice());
+                $this->entityManager->persist($orderDetails);
+            }
+
+            $this->entityManager->flush();
+
+            return $this->render('order/add.html.twig', [
+                'cart' => $cart->getFull(),
+                'moto' => $moto_content,
 //                'reference' => $order->getReference(),
-//            ]);
-//        }
-//
-//        //sinon renvoi vers le panier
-//        return $this->redirectToRoute('cart');
-//    }
+            ]);
+        }
+
+        //sinon renvoi vers le panier
+        return $this->redirectToRoute('cart');
+    }
 }
