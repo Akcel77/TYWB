f7eb10d4f54b4049395a775338b85f7025195e26
Akcel77 <Akeucel@gmail.com>
2022-08-20 00:56:22 +0200

    SQL check description

---
+++ migrations/Version20220819215822.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220819215822 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Add passengers table to Order Entity';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE `order` ADD passengers INT NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE `order` DROP passengers');
+    }
+}
