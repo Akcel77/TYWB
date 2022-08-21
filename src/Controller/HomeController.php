95890463e5d68d7f86365d77e92081bd98a8af84
Akcel77 <Akeucel@gmail.com>
2022-08-21 14:50:56 +0200

    Get isBest ride to HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,18 +3,31 @@
 namespace App\Controller;
 
 use App\Classe\Cart;
+use App\Entity\Ride;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * @Route("/", name="home")
      */
     public function index(Cart $cart): Response
     {
+        $rides = $this->entityManager->getRepository(Ride::class)->findBy(['isBest' => 1]);
+
         return $this->render('home/index.html.twig', [
+            'rides' => $rides,
             'cart' => $cart->getFull(),
         ]);
     }
