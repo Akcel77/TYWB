b5db490efca00c91579b3f2ba63b243ab8279c2c
Akcel77 <Akeucel@gmail.com>
2022-08-22 22:58:29 +0200

    Set show to RideCrudController

--- src/Controller/Admin/RideCrudController.php
+++ src/Controller/Admin/RideCrudController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Ride;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
@@ -19,6 +20,12 @@ class RideCrudController extends AbstractCrudController
     public static function getEntityFqcn(): string
     {
         return Ride::class;
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->add('index', 'detail');
     }
 
 
